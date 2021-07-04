cc084122aebfeac9c8cd022b570fd046ef78aa45
kkansaria <kansariakartik81@gmail.com>
2021-07-04 10:04:51 -0400

    Delete prediction_model directory

--- prediction_model/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# admin.site.register(patient)
-# admin.site.register(doctor)
-# admin.site.register(diseaseinfo)
-# admin.site.register(consultation)
-# admin.site.register(rating_review)
